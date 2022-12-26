44ff5502ced56b3287fc365268ce9619224acb5d
ashish <20190802091@dypiu.ac.in>
2022-12-27 03:00:24 +0530

    async function call parall

--- 0.practice.js
+++ 0.practice.js
@@ -1,21 +1,47 @@
-const asyncSeriesExecuter_reducer = function(promises) {
-  promises.reduce((acc, curr) => {
-  return acc.then(() => {
-  return curr.then(val => {console.log(val)});
+const wait = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(), ms);
   });
-  }, Promise.resolve());
-  }
 
-const asyncTask = (taskNum) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`Task ${taskNum} has been completed`);
-    }, taskNum * 100);
-  });
+const retryWithDelay = async (
+  fn,
+  retries = 3,
+  interval = 50,
+  finalErr = "Retry failed"
+) => {
+  try {
+    // try
+    await fn();
+  } catch (err) {
+    // if no retries left
+    // throw error
+    if (retries <= 0) {
+      return Promise.reject(finalErr);
+    }
+    //delay the next call
+    await wait(interval);
+    //recursively call the same func
+    return retryWithDelay(fn, retries - 1, interval, finalErr);
+  }
 };
 
-const promiseList = [asyncTask(3), asyncTask(10), asyncTask(9), asyncTask(4)];
-
-asyncSeriesExecuter_reducer(promiseList);
-
-
+const getTestFunc = () => {
+  let callCounter = 0;
+  return async () => {
+    callCounter += 1;
+    // if called less than 5 times
+    // throw error
+    if (callCounter < 5) {
+      throw new Error("Not yet");
+    }
+  };
+};
+// Test the code
+const test = async () => {
+  await retryWithDelay(getTestFunc(), 10);
+  console.log("success");
+  await retryWithDelay(getTestFunc(), 3);
+  console.log("will fail before getting here");
+};
+// Print the result
+test().catch(console.error);
