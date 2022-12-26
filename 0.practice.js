32b4870f0a24553f88f35b1a19073fdbccf8263a
ashish <20190802091@dypiu.ac.in>
2022-12-26 20:15:08 +0530

    series and parallel execution of async functions

--- 0.practice.js
+++ 0.practice.js
@@ -1,133 +1,21 @@
-// enum of states
-const states = {
-  PENDING: 0,
-  FULFILLED: 1,
-  REJECTED: 2,
+const asyncSeriesExecuter_reducer = function(promises) {
+  promises.reduce((acc, curr) => {
+  return acc.then(() => {
+  return curr.then(val => {console.log(val)});
+  });
+  }, Promise.resolve());
+  }
+
+const asyncTask = (taskNum) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Task ${taskNum} has been completed`);
+    }, taskNum * 100);
+  });
 };
 
-class MyPromise {
-  // initialize the promise
-  constructor(callback) {
-    this.state = states.PENDING;
-    this.value = undefined;
-    this.handlers = [];
-    try {
-      callback(this._resolve, this._reject);
-    } catch (error) {
-      this._reject(error);
-    }
-  }
-  // helper function for resolve
-  _resolve = (value) => {
-    this._handleUpdate(states.FULFILLED, value);
-  };
-  // helper function for reject
-  _reject = (value) => {
-    this._handleUpdate(states.REJECTED, value);
-  };
-  // handle the state change
-  _handleUpdate = (state, value) => {
-    if (state === states.PENDING) {
-      return;
-    }
-    setTimeout(() => {
-      if (value instanceof MyPromise) {
-        value.then(this._resolve, this._reject);
-      }
-      this.state = state;
-      this.value = value;
-      this._executeHandlers();
-    }, 0);
-  };
-  // execute all the handlers
-  // depending on the current state
-  _executeHandlers = () => {
-    if (this.state === states.PENDING) {
-      return;
-    }
-    this.handlers.forEach((handler) => {
-      if (this.state === states.FULFILLED) {
-        return handler.onSuccess(this.value);
-      }
-      return handler.onFailure(this.value);
-    });
-    this.handlers = [];
-  };
-  // add handlers
-  // execute all if any new handler is added
-  _addHandler = (handler) => {
-    this.handlers.push(handler);
-    this._executeHandlers();
-  };
-  // then handler
-  // creates a new promise
-  // assigned the handler
-  then = (onSuccess, onFailure) => {
-    // invoke the constructor
-    // and new handler
-    return new MyPromise((resolve, reject) => {
-      this._addHandler({
-        onSuccess: (value) => {
-          if (!onSuccess) {
-            return resolve(value);
-          }
-          try {
-            return resolve(onSuccess(value));
-          } catch (error) {
-            reject(error);
-          }
-        },
-        onFailure: (value) => {
-          if (!onFailure) {
-            return reject(value);
-          }
-          try {
-            return reject(onFailure(value));
-          } catch (error) {
-            return reject(error);
-          }
-        },
-      });
-    });
-  };
-  // add catch handler
-  catch = (onFailure) => {
-    return this.then(null, onFailure);
-  };
-  // add the finally handler
-  finally = (callback) => {
-    // create a new constructor
-    // listen the then and catch method
-    // finally perform the action
-    return new MyPromise((resolve, reject) => {
-      let wasResolved;
-      let value;
-      this.then((val) => {
-        value = val;
-        wasResolved = true;
-        return callback();
-      }).catch((err) => {
-        value = err;
-        wasResolved = false;
-        return callback();
-      });
-      if (wasResolved) {
-        resolve(value);
-      } else {
-        reject(value);
-      }
-    });
-  };
-}
+const promiseList = [asyncTask(3), asyncTask(10), asyncTask(9), asyncTask(4)];
 
-// Input:
-const promise = new MyPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("hello");
-  }, 1000);
-});
-promise.then((value) => {
-  console.log(value);
-});
-// Output:
-// "hello"
+asyncSeriesExecuter_reducer(promiseList);
+
+
